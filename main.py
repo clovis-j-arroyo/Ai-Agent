3980f3f0fb61a2c3d0ee4c78682c8856e2fb8e50
Cjarroyo <clovis_arroyo@hotmail.com>
2026-02-14 00:19:20 -0300

    feat: added types for python conversion and a --verbose so you can see or not the prompt stats

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 def main():
@@ -13,16 +14,21 @@ def main():
     client = genai.Client(api_key=api_key)
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
-    response = client.models.generate_content(model="gemini-2.5-flash",contents=args.user_prompt)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",contents=messages
+        )
     
     if response.usage_metadata is None:
         raise RuntimeError("Oh no, the Ai has gone to sleep for now, try again in a few moments.")
-    
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+    if args.verbose == True:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
 if __name__ == "__main__":
     main()
