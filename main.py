82988327615f3443dc8177e4e75d64289ab67a59
Cjarroyo <clovis_arroyo@hotmail.com>
2026-03-09 08:05:07 -0300

    feat: updated main.py to use the call function and be able to have conversations

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from google import genai
 from google.genai import types
 import argparse
 from prompts import system_prompt
-from functions.call_function import available_functions
+from functions.call_function import available_functions, call_function
+import  sys
 
 
 def main():
@@ -22,28 +23,52 @@ def main():
     args = parser.parse_args()
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt,
-            temperature=0)
-        )
-    
-    if response.usage_metadata is None:
-        raise RuntimeError("Oh no, the Ai has gone to sleep for now, try again in a few moments.")
-    if args.verbose == True:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
-
-    if response.function_calls is not None:
-        for function_call in response.function_calls:
-            print(f"Calling function: {function_call.name}({function_call.args})")
-    else:
-        print(f"Response:\n{response.text}")
+    for i in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions],
+                system_instruction=system_prompt,
+                temperature=0)
+            )
         
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+
+        if response.usage_metadata is None:
+            raise RuntimeError("Oh no, the Ai has gone to sleep for now, try again in a few moments.")
+        if args.verbose == True:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens:{response.usage_metadata.candidates_token_count}")
+
+        function_responses = []
+        if response.function_calls is not None:
+            for function_call in response.function_calls:
+                result = call_function(function_call, args.verbose)
+                if not result.parts:
+                    raise RuntimeError("There is missing parts")
+                if not result.parts[0].function_response:
+                    raise RuntimeError("There is missing parts")
+                if not result.parts[0].function_response.response:
+                    raise RuntimeError("There is missing parts")
+                if args.verbose:
+                    print(f"-> {result.parts[0].function_response.response}")
+                function_responses.append(result.parts[0])
+
+        if  function_responses:
+            messages.append(types.Content(role="user", parts=function_responses))
+
+        else:
+            print(f"Response:\n{response.text}")
+            break
+
+    else:
+        print("The agent has reached its current limit thank you for using my Ai Agent.")
+        sys.exit(1)
+
 if __name__ == "__main__":
     main()
